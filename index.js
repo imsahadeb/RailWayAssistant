86f5c1b08d3a655ad9c98a840f291508537937bb
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-18 11:38:32 +0530

    replace axios with request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express =require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+const request_data= require('request');
 const app = express();
 const API_KEY = "q86si59pft";
 app.use(
@@ -16,19 +17,29 @@ app.post('/api',(request,res)=>{
     //     'fulfillmentText':"train_name"
     // });
     let find_train_url='https://api.railwayapi.com/v2/name-number/train/'+train+'/apikey/q86si59pft/';
-    axios.get(find_train_url)
-       .then(response=>{
-           //response
-           let info=response.data;
-           let train_name=info.train['name'];
-           //console.log(train_name);
-           res.status(200).json({
+    // axios.get(find_train_url)
+    //    .then(response=>{
+    //        //response
+    //        let info=response.data;
+    //        let train_name=info.train['name'];
+    //        //console.log(train_name);
+    //        res.status(200).json({
+    //         'fulfillmentText':train_name
+    //          });
+    //    })
+    //    .catch(error=>{
+    //        console.log(error);
+    //    });
+    request_data(find_train_url,(req,response,body)=>{
+        let info= JSON.parse(body);
+        let train_name=info.train['name'];
+        console.log(train_name);
+        res.status(200).json({
             'fulfillmentText':train_name
-             });
-       })
-       .catch(error=>{
-           console.log(error);
-       });
+        });
+    });
+
+
 });
 
 app.listen(process.env.PORT || 80);
