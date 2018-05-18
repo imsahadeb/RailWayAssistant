829bab488cd4ad9896d900c0eef9ddb04086b436
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-18 11:03:12 +0530

    hello

--- index.js
+++ index.js
@@ -11,26 +11,14 @@ app.use(
   app.use(bodyParser.json());
 
 app.post('/api',(request,res)=>{
-    
     let train = request.body.queryResult.parameters['train-no'];
-   // let city = request.body.queryResult.parameters['geo-city'];
-    //train="13147";
-    console.log("Train No: "+train);
-    //let train=post_body.x
-   // let url='https://api.railwayapi.com/v2/route/train/'+train+'/apikey/'+API_KEY+'/';
     let find_train_url='https://api.railwayapi.com/v2/name-number/train/'+train+'/apikey/q86si59pft/';
-   // console.log("requested URL: "+url);
     axios.get(find_train_url)
        .then(response=>{
            //response
            let info=response.data;
-          // var len=Object.keys(info['route']).length;
-
            let train_name=info.train['name'];
-        //    let s=info['route'][0]['station'];
-        //    var outResult="";
-       
-           console.log(info);
+           console.log(train_name);
            res.status(200).json({
                'fulfillmentText':train_name
              })
