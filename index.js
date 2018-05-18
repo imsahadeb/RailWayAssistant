f3e32c5bf86b891cbd30a1d9a462a609170f8d73
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-18 12:46:10 +0530

    testing

--- index.js
+++ index.js
@@ -14,18 +14,19 @@ app.use(
 app.post('/api',(request,res)=>{
     let train = request.body.queryResult.parameters['train_no'];
 
-    // let find_train_url='https://api.railwayapi.com/v2/name-number/train/'+train+'/apikey/q86si59pft/'
-    // request_data(find_train_url,(req,response,body)=>{
-    //     let info= JSON.parse(body);
-    //     let train_name="sahadeb";
-    //     console.log(train_name);
-    //     res.json({
-    //         'fulfillmentText':"Name of the  train is: " +train_name
-    //     });
-    // });
-    res.json({
-        'fulfillmentText':train
-    })
+    let find_train_url='https://api.railwayapi.com/v2/name-number/train/'+train+'/apikey/q86si59pft/'
+    request_data(find_train_url,(req,response,body)=>{
+        let info= JSON.parse(body);
+        let train_name=info['name'];
+        console.log(train_name);
+        res.json({
+            'fulfillmentText':"Name of the  train is: " +train_name
+        });
+
+    });
+    // res.json({
+    //     'fulfillmentText':train
+    // })
 
 
 });
