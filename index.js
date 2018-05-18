366514a1b22f4402f4445925c3cee94c55d7f434
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-18 13:06:57 +0530

    testing

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 const request_data= require('request');
 const app = express();
-const API_KEY = "q86si59pft";
+const API_KEY1= "q86si59pft";
+const API_KEY2 = "ye1rpmx0tk";
 app.use(
     bodyParser.urlencoded({
       extended: true
@@ -14,13 +15,14 @@ app.use(
 app.post('/api',(request,res)=>{
     let train = request.body.queryResult.parameters['train_no'];
 
-    let find_train_url='https://api.railwayapi.com/v2/name-number/train/'+train+'/apikey/q86si59pft/'
+    let find_train_url='https://api.railwayapi.com/v2/name-number/train/'+train+'/apikey/'+API_KEY2+'/'
     request_data(find_train_url,(req,response,body)=>{
+        console.log(find_train_url);
         let info= JSON.parse(body);
-        let train_name=info['name'];
+        let train_name=info.train['name'];
         console.log(train_name);
         res.json({
-            'fulfillmentText':"Name of the  train is: " +train_name
+            'fulfillmentText':"Name of the  train is: " + train_name
         });
 
     });
