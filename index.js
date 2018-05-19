f79ca21ba084d2fa47ac751b10ac7a81b4810757
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-19 23:33:55 +0530

    Rich msg impimention

--- index.js
+++ index.js
@@ -66,45 +66,8 @@ app.post('/api',(request,res)=>{
                       ]
                     }
                   }
-                ],
-                "source": "example.com",
-                "payload": {
-                  "google": {
-                    "expectUserResponse": true,
-                    "richResponse": {
-                      "items": [
-                        {
-                          "simpleResponse": {
-                            "textToSpeech": "this is a simple response"
-                          }
-                        }
-                      ]
-                    }
-                  },
-                  "facebook": {
-                    "text": "Hello, Facebook!"
-                  },
-                  "slack": {
-                    "text": "This is a text response for Slack."
-                  }
-                },
-                "outputContexts": [
-                  {
-                    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-                    "lifespanCount": 5,
-                    "parameters": {
-                      "param": "param value"
-                    }
-                  }
-                ],
-                "followupEventInput": {
-                  "name": "event name",
-                  "languageCode": "en-US",
-                  "parameters": {
-                    "param": "param value"
-                  }
-                }
-
+                ]
+                
 
         });
         res.end();
