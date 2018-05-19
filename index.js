8db0c93108e5ed60918b1046ac6285435a4e17a9
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-19 23:29:04 +0530

    Rich msg impimention

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/api',(request,res)=>{
     var find_position_by_no=request.body.queryResult.parameters['find_position_by_no'];
 
     if(find_position_by_no){
-        let find_train_url='https://api.railwayapi.com/v2/live/train/'+find_position_by_no+'/date/18-05-2018/apikey/'+API_KEY2+'/'
+        let find_train_url='https://api.railwayapi.com/v2/live/train/'+find_position_by_no+'/date/'+date+'/apikey/'+API_KEY2+'/'
         request_data(find_train_url,(req,response,body)=>{
         console.log(find_train_url);
         let info= JSON.parse(body);
@@ -47,8 +47,65 @@ app.post('/api',(request,res)=>{
         let train_name=info.train['name'];
         console.log(train_name);
         res.status(200).json({
-            'fulfillmentText':"The name of the Train Number " +info.train['number']+ ' is '
-             +replace.all("Exp","Express").from(changeCase.titleCase(info.train['name'])).with("Express")
+            // 'fulfillmentText':"The name of the Train Number " +info.train['number']+ ' is '
+            //  +replace.all("Exp","Express").from(changeCase.titleCase(info.train['name'])).with("Express")
+
+            
+                "fulfillmentText": "This is a text response",
+                "fulfillmentMessages": [
+                  {
+                    "card": {
+                      "title": "card title",
+                      "subtitle": "card text",
+                      "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+                      "buttons": [
+                        {
+                          "text": "button text",
+                          "postback": "https://assistant.google.com/"
+                        }
+                      ]
+                    }
+                  }
+                ],
+                "source": "example.com",
+                "payload": {
+                  "google": {
+                    "expectUserResponse": true,
+                    "richResponse": {
+                      "items": [
+                        {
+                          "simpleResponse": {
+                            "textToSpeech": "this is a simple response"
+                          }
+                        }
+                      ]
+                    }
+                  },
+                  "facebook": {
+                    "text": "Hello, Facebook!"
+                  },
+                  "slack": {
+                    "text": "This is a text response for Slack."
+                  }
+                },
+                "outputContexts": [
+                  {
+                    "name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+                    "lifespanCount": 5,
+                    "parameters": {
+                      "param": "param value"
+                    }
+                  }
+                ],
+                "followupEventInput": {
+                  "name": "event name",
+                  "languageCode": "en-US",
+                  "parameters": {
+                    "param": "param value"
+                  }
+                }
+
+
         });
         res.end();
 
