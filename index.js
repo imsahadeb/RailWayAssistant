09382bea4d0da1130765fb7074cdbbc580af5cc0
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-25 12:15:16 +0530

    constructing file structure of the project

--- index.js
+++ index.js
@@ -137,72 +137,6 @@ app.post('/api',(request,res)=>{
             res.status(200).json({
                
                     "payload": {
-                      "facebook": {
-                        "attachment": {
-                          "type": "template",
-                          "payload": {
-                            "template_type": "airline_boardingpass",
-                            "intro_message": "Current Seat availibility",
-                            "locale": "en_US",
-                            "boarding_pass": [
-                              {
-                                "passenger_name": "SMITH\/NICOLAS",
-                                "pnr_number": "CG4X7U",
-                                "seat": "74J",            
-                                "logo_image_url": "https:\/\/www.example.com\/en\/logo.png",
-                                "header_image_url": "https:\/\/www.example.com\/en\/fb\/header.png",
-                                "qr_code": "M1SMITH\/NICOLAS  CG4X7U nawouehgawgnapwi3jfa0wfh",
-                                "above_bar_code_image_url": "https:\/\/www.example.com\/en\/PLAT.png",
-                                "auxiliary_fields": [
-                                  {
-                                    "label": "Terminal",
-                                    "value": "T1"
-                                  },
-                                  {
-                                    "label": "Departure",
-                                    "value": "30OCT 19:05"
-                                  }
-                                ],
-                                "secondary_fields": [
-                                  {
-                                    "label": "Boarding",
-                                    "value": "18:30"
-                                  },
-                                  {
-                                    "label": "Gate",
-                                    "value": "D57"
-                                  },
-                                  {
-                                    "label": "Seat",
-                                    "value": "74J"
-                                  },
-                                  {
-                                    "label": "Sec.Nr.",
-                                    "value": "003"
-                                  }
-                                ],
-                                "flight_info": {
-                                  "flight_number": "KL0642",
-                                  "departure_airport": {
-                                    "airport_code": "JFK",
-                                    "city": "New York",
-                                    "terminal": "T1",
-                                    "gate": "D57"
-                                  },
-                                  "arrival_airport": {
-                                    "airport_code": "AMS",
-                                    "city": "Amsterdam"
-                                  },
-                                  "flight_schedule": {
-                                    "departure_time": "2016-01-02T19:05",
-                                    "arrival_time": "2016-01-05T17:30"
-                                  }
-                                }
-                              }
-                            ]
-                          }
-                        }
-                      },
                       "google": {
                         "expectUserResponse": true,
                         "richResponse": {
@@ -290,7 +224,7 @@ app.post('/api',(request,res)=>{
     });
     }
 
-   if(intent=='find train name'){
+   if(intent=='find train name - yes'){
      console.log(parameters['train_no']);
     var train_no = parameters['train_no'].trim();
     console.log(train_no);
