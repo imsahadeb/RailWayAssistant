194257f618a496b76d451626119f1542ee7532a5
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-26 09:39:58 +0530

    testig

--- API/module/RequestType/getTrainLiveStatus.js
+++ API/module/RequestType/getTrainLiveStatus.js
@@ -5,8 +5,8 @@ const getDataFromConstantFile = require('../../../constraints/constant');
 
 module.exports.getLiveTrainStatus= function getLiveTrainStatus(request,passtoHandler){
     let parameters = request.body.queryResult.parameters;
-    let TRAIN_NO = parameters['train_no'];
-    let TRAIN_DATE ='25-05-2018';
+    let TRAIN_NO = parameters['TRAIN_NO'];
+    let TRAIN_DATE =moment(parameters.DATE,'YYYY-DD-MM').format('DD-MM-YYYY');
     let url = getDataFromConstantFile.API_HOST + '/v2/live/train/'+TRAIN_NO+'/date/'
     +TRAIN_DATE+'/apikey/'+getDataFromConstantFile.API_KEY_1 +'/';
     console.log("Requested Url For Live Train: "+url);
