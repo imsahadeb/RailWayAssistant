090a359d936597dc39b77e2aa0af8df676fd432d
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-28 15:52:17 +0530

    test

--- API/RequestHandler/proceedUserRequest.js
+++ API/RequestHandler/proceedUserRequest.js
@@ -179,39 +179,31 @@ function sendDefaultResponse(request,passtoHandler){
 
       var tableCard={
         "payload": {
-            "facebook" : {
-                "attachment":{
-                    "type":"template",
-                    "payload":{
-                      "template_type":"generic",
-                      "elements":[
-                         {
-                          "title":"Welcome!",
-                          "image_url":"https://petersfancybrownhats.com/company_image.png",
-                          "subtitle":"We have the right hat for everyone.",
-                          "default_action": {
-                            "type": "web_url",
-                            "url": "https://petersfancybrownhats.com/view?item=103",
-                            "messenger_extensions": true,
-                            "webview_height_ratio": "tall",
-                            "fallback_url": "https://petersfancybrownhats.com/"
-                          },
-                          "buttons":[
-                            {
-                              "type":"web_url",
-                              "url":"https://petersfancybrownhats.com",
-                              "title":"View Website"
-                            },{
-                              "type":"postback",
-                              "title":"Start Chatting",
-                              "payload":"DEVELOPER_DEFINED_PAYLOAD"
-                            }              
-                          ]      
+            
+                "facebook": {
+                    "attachment": {
+                        "type": "template",
+                        "payload": {
+                            "template_type": "generic",
+                            "elements": 
+                            [
+                                {
+                                    "title": "sahadeb",
+                                    "image_url": url,
+                                    "buttons":
+                                    [
+                                        {
+                                            "type": "postback",
+                                            "title": `BUTTON_TITLE`,
+                                            "payload": `<PAYLOAD>`
+                                        }
+                                    ]
+                                }
+                            ]
                         }
-                      ]
                     }
-                  }
                 },
+            
           "google": {
             "expectUserResponse": true,
             "richResponse": {
