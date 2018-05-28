60cfeeb517662d3e01e0de4ca8fc212fe5632dbe
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-28 16:04:20 +0530

    test

--- API/RequestHandler/proceedUserRequest.js
+++ API/RequestHandler/proceedUserRequest.js
@@ -297,32 +297,40 @@ function sendDefaultResponse(request,passtoHandler){
     //   //passtoHandler(tableCard);
     
 
-    var facebook = {
-        'data': {
-            "facebook": {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": 
-                        [
-                            {
-                                "title": 'title',
-                                "image_url": 'url',
-                                "buttons":
-                                [
-                                    {
-                                        "type": "postback",
-                                        "title": `BUTTON_TITLE`,
-                                        "payload": `<PAYLOAD>`
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                }
-            }
+    var facebook ={
+     "message":{
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"generic",
+            "elements":[
+               {
+                "title":"Welcome!",
+                "image_url":"https://petersfancybrownhats.com/company_image.png",
+                "subtitle":"We have the right hat for everyone.",
+                "default_action": {
+                  "type": "web_url",
+                  "url": "https://petersfancybrownhats.com/view?item=103",
+                  "messenger_extensions": false,
+                  "webview_height_ratio": "tall",
+                  "fallback_url": "https://petersfancybrownhats.com/"
+                },
+                "buttons":[
+                  {
+                    "type":"web_url",
+                    "url":"https://petersfancybrownhats.com",
+                    "title":"View Website"
+                  },{
+                    "type":"postback",
+                    "title":"Start Chatting",
+                    "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                  }              
+                ]      
+              }
+            ]
+          }
         }
+      }
     }
      passtoHandler(facebook);
 }
