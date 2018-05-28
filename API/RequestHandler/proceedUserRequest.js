372d5fe23c44924a92fa7f9c4ecb37d9e684ab36
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-28 16:00:54 +0530

    test

--- API/RequestHandler/proceedUserRequest.js
+++ API/RequestHandler/proceedUserRequest.js
@@ -68,233 +68,233 @@ function (request,passtoHandler){
 
 function sendDefaultResponse(request,passtoHandler){
 
-    var results={
-        "payload": {
-          "google": {
-            "expectUserResponse": true,
-            "richResponse": {
-              "items": [
-                {
-                  "simpleResponse": {
-                    "textToSpeech": "Choose a item"
-                  }
-                }
-              ]
-            },
-            "systemIntent": {
-              "intent": "actions.intent.OPTION",
-              "data": {
-                "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                "listSelect": {
-                  "title": "Train List",
-                  "items": [
-                    {
-                      "title": "Train No: 13148, Uttarbango Express",
-                      "description": "Actual Arrival Time: 3:15 pm, is late by 30 min",  
-                      "optionInfo": {
-                        "key": "first title key"
-                      }
+    // var results={
+    //     "payload": {
+    //       "google": {
+    //         "expectUserResponse": true,
+    //         "richResponse": {
+    //           "items": [
+    //             {
+    //               "simpleResponse": {
+    //                 "textToSpeech": "Choose a item"
+    //               }
+    //             }
+    //           ]
+    //         },
+    //         "systemIntent": {
+    //           "intent": "actions.intent.OPTION",
+    //           "data": {
+    //             "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
+    //             "listSelect": {
+    //               "title": "Train List",
+    //               "items": [
+    //                 {
+    //                   "title": "Train No: 13148, Uttarbango Express",
+    //                   "description": "Actual Arrival Time: 3:15 pm, is late by 30 min",  
+    //                   "optionInfo": {
+    //                     "key": "first title key"
+    //                   }
                       
                      
-                    },
-                    {
-                      "optionInfo": {
-                        "key": "second"
-                      },
-                      "description": "Actual Arrival Time: 5:15pm, is on Right Time",
+    //                 },
+    //                 {
+    //                   "optionInfo": {
+    //                     "key": "second"
+    //                   },
+    //                   "description": "Actual Arrival Time: 5:15pm, is on Right Time",
                       
-                      "title": "Train No: 12345, Saraighat Express"
-                    }
-                  ]
-                }
-              }
-            }
-          }
-        }
-      }
+    //                   "title": "Train No: 12345, Saraighat Express"
+    //                 }
+    //               ]
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //   }
 
 
-      var ob={
-        "payload": {
-          "google": {
-            "expectUserResponse": true,
-            "richResponse": {
-              "items": [
-                {
-                  "simpleResponse": {
-                    "textToSpeech": "Train List"
-                  }
-                },
-                {
-                  "tableCard": {
-                    "rows": [
-                      {
-                        "cells": [
-                          {
-                            "text": "row 1 item 1"
-                          },
-                          {
-                            "text": "row 1 item 2"
-                          },
-                          {
-                            "text": "row 1 item 3"
-                          }
-                        ],
-                        "dividerAfter": true
-                      },
-                      {
-                        "cells": [
-                          {
-                            "text": "row 2 item 1"
-                          },
-                          {
-                            "text": "row 2 item 2"
-                          },
-                          {
-                            "text": "row 2 item 3"
-                          }
-                        ],
-                        "dividerAfter": true
-                      }
-                    ],
-                    "columnProperties": [
-                      {
-                        "header": "header 1"
-                      },
-                      {
-                        "header": "header 2"
-                      },
-                      {
-                        "header": "header 3"
-                      }
-                    ]
-                  }
-                }
-              ]
-            },
-            "userStorage": "{\"data\":{}}"
-          }
-        }
-      }
+    //   var ob={
+    //     "payload": {
+    //       "google": {
+    //         "expectUserResponse": true,
+    //         "richResponse": {
+    //           "items": [
+    //             {
+    //               "simpleResponse": {
+    //                 "textToSpeech": "Train List"
+    //               }
+    //             },
+    //             {
+    //               "tableCard": {
+    //                 "rows": [
+    //                   {
+    //                     "cells": [
+    //                       {
+    //                         "text": "row 1 item 1"
+    //                       },
+    //                       {
+    //                         "text": "row 1 item 2"
+    //                       },
+    //                       {
+    //                         "text": "row 1 item 3"
+    //                       }
+    //                     ],
+    //                     "dividerAfter": true
+    //                   },
+    //                   {
+    //                     "cells": [
+    //                       {
+    //                         "text": "row 2 item 1"
+    //                       },
+    //                       {
+    //                         "text": "row 2 item 2"
+    //                       },
+    //                       {
+    //                         "text": "row 2 item 3"
+    //                       }
+    //                     ],
+    //                     "dividerAfter": true
+    //                   }
+    //                 ],
+    //                 "columnProperties": [
+    //                   {
+    //                     "header": "header 1"
+    //                   },
+    //                   {
+    //                     "header": "header 2"
+    //                   },
+    //                   {
+    //                     "header": "header 3"
+    //                   }
+    //                 ]
+    //               }
+    //             }
+    //           ]
+    //         },
+    //         "userStorage": "{\"data\":{}}"
+    //       }
+    //     }
+    //   }
 
-      var tableCard={
-        "payload": {
+    //   var tableCard={
+    //     "payload": {
             
-                "facebook": {
-                    "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "generic",
-                            "elements": 
-                            [
-                                {
-                                    "title": "sahadeb",
-                                    "image_url": url,
-                                    "buttons":
-                                    [
-                                        {
-                                            "type": "postback",
-                                            "title": `BUTTON_TITLE`,
-                                            "payload": `<PAYLOAD>`
-                                        }
-                                    ]
-                                }
-                            ]
-                        }
-                    }
-                },
+    //             "facebook": {
+    //                 "attachment": {
+    //                     "type": "template",
+    //                     "payload": {
+    //                         "template_type": "generic",
+    //                         "elements": 
+    //                         [
+    //                             {
+    //                                 "title": "sahadeb",
+    //                                 "image_url": url,
+    //                                 "buttons":
+    //                                 [
+    //                                     {
+    //                                         "type": "postback",
+    //                                         "title": `BUTTON_TITLE`,
+    //                                         "payload": `<PAYLOAD>`
+    //                                     }
+    //                                 ]
+    //                             }
+    //                         ]
+    //                     }
+    //                 }
+    //             },
             
-          "google": {
-            "expectUserResponse": true,
-            "richResponse": {
-              "items": [
-                {
-                  "simpleResponse": {
-                    "textToSpeech": "Here is the Train List"
-                  }
-                },
-                {
-                  "tableCard": {
-                    "title": "Train List",
-                    "subtitle": "Table Subtitle",
-                    "image": {
-                      "url": "https://avatars0.githubusercontent.com/u/23533486",
-                      "accessibilityText": "Actions on Google"
-                    },
-                    "rows": [
-                      {
-                        "cells": [
-                          {
-                            "text": "row 1 item 1"
-                          },
-                          {
-                            "text": "row 1 item 2"
-                          },
-                          {
-                            "text": "row 1 item 3"
-                          }
-                        ],
-                        "dividerAfter": false
-                      },
-                      {
-                        "cells": [
-                          {
-                            "text": "row 2 item 1"
-                          },
-                          {
-                            "text": "row 2 item 2"
-                          },
-                          {
-                            "text": "row 2 item 3"
-                          }
-                        ],
-                        "dividerAfter": true
-                      },
-                      {
-                        "cells": [
-                          {
-                            "text": "row 2 item 1"
-                          },
-                          {
-                            "text": "row 2 item 2"
-                          },
-                          {
-                            "text": "row 2 item 3"
-                          }
-                        ]
-                      }
-                    ],
-                    "columnProperties": [
-                      {
-                        "header": "header 1",
-                        "horizontalAlignment": "CENTER"
-                      },
-                      {
-                        "header": "header 2",
-                        "horizontalAlignment": "LEADING"
-                      },
-                      {
-                        "header": "header 3",
-                        "horizontalAlignment": "TRAILING"
-                      }
-                    ],
-                    "buttons": [
-                      {
-                        "title": "Button Title",
-                        "openUrlAction": {
-                          "url": "https://github.com/actions-on-google"
-                        }
-                      }
-                    ]
-                  }
-                }
-              ]
-            },
-            "userStorage": "{\"data\":{}}"
-          }
-        }
-      }
-      //passtoHandler(tableCard);
+    //       "google": {
+    //         "expectUserResponse": true,
+    //         "richResponse": {
+    //           "items": [
+    //             {
+    //               "simpleResponse": {
+    //                 "textToSpeech": "Here is the Train List"
+    //               }
+    //             },
+    //             {
+    //               "tableCard": {
+    //                 "title": "Train List",
+    //                 "subtitle": "Table Subtitle",
+    //                 "image": {
+    //                   "url": "https://avatars0.githubusercontent.com/u/23533486",
+    //                   "accessibilityText": "Actions on Google"
+    //                 },
+    //                 "rows": [
+    //                   {
+    //                     "cells": [
+    //                       {
+    //                         "text": "row 1 item 1"
+    //                       },
+    //                       {
+    //                         "text": "row 1 item 2"
+    //                       },
+    //                       {
+    //                         "text": "row 1 item 3"
+    //                       }
+    //                     ],
+    //                     "dividerAfter": false
+    //                   },
+    //                   {
+    //                     "cells": [
+    //                       {
+    //                         "text": "row 2 item 1"
+    //                       },
+    //                       {
+    //                         "text": "row 2 item 2"
+    //                       },
+    //                       {
+    //                         "text": "row 2 item 3"
+    //                       }
+    //                     ],
+    //                     "dividerAfter": true
+    //                   },
+    //                   {
+    //                     "cells": [
+    //                       {
+    //                         "text": "row 2 item 1"
+    //                       },
+    //                       {
+    //                         "text": "row 2 item 2"
+    //                       },
+    //                       {
+    //                         "text": "row 2 item 3"
+    //                       }
+    //                     ]
+    //                   }
+    //                 ],
+    //                 "columnProperties": [
+    //                   {
+    //                     "header": "header 1",
+    //                     "horizontalAlignment": "CENTER"
+    //                   },
+    //                   {
+    //                     "header": "header 2",
+    //                     "horizontalAlignment": "LEADING"
+    //                   },
+    //                   {
+    //                     "header": "header 3",
+    //                     "horizontalAlignment": "TRAILING"
+    //                   }
+    //                 ],
+    //                 "buttons": [
+    //                   {
+    //                     "title": "Button Title",
+    //                     "openUrlAction": {
+    //                       "url": "https://github.com/actions-on-google"
+    //                     }
+    //                   }
+    //                 ]
+    //               }
+    //             }
+    //           ]
+    //         },
+    //         "userStorage": "{\"data\":{}}"
+    //       }
+    //     }
+    //   }
+    //   //passtoHandler(tableCard);
     
 
     var facebook = {
