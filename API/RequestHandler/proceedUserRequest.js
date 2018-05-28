28d31d3831b91093b98e62f4aa9ce7687e0e5810
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-28 10:25:03 +0530

    test

--- API/RequestHandler/proceedUserRequest.js
+++ API/RequestHandler/proceedUserRequest.js
@@ -76,12 +76,14 @@ function sendDefaultResponse(request,passtoHandler){
                        "items": [
                          {
                            "simpleResponse": {
-                             "textToSpeech": "Here is the suggestions for you"
+                             "textToSpeech": "Here is the suggestions for you",
+                             'diaplayText': 'hello hi this is smaple text, hindn dhkjxdbkbkbb  dhbhbjdb jbcjbdj'
                            }
                          },
                          {
                            "simpleResponse": {
-                             "textToSpeech": "You can click on any suggestions for forther assistant."
+                             "textToSpeech": "You can click on any suggestions for forther assistant.",
+                             'dispalyText': 'hdbshdjksdks jhfjhdsfhsdh hjhsjdhdsj hjhs jjshj jhjhsdh'
                            }
                          },
                          {
