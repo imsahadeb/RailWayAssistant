2a763026afe55a5f94634a064e5309f5d891d92b
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-29 11:40:00 +0530

    testing

--- API/module/RequestType/getPnrStatus.js
+++ API/module/RequestType/getPnrStatus.js
@@ -7,7 +7,7 @@ const getDataFromConstantFile = require('../../../constraints/constant');
 module.exports.getPnrStatus = function getPnrStatus(request,passtoHandler){
     let parameters =request.body.queryResult.parameters;
     let PNR_NO =parameters.PNR_NO;
-    let URL = getDataFromConstantFile.API_HOST + '/v2/pnr_status/pnr/' + PNR + '/apikey/'
+    let URL = getDataFromConstantFile.API_HOST + '/v2/pnr-status/pnr/' + PNR + '/apikey/'
      + getDataFromConstantFile.API_KEY_1 + '/';
      fromRailWayAPI.callTheRailwayApi(URL,(getResponseFromAPI)=>{
         let getJsonData = JSON.parse(getResponseFromAPI);
