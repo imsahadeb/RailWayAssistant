0fd16a7df393d88acd1082b0be2560c5e3ac3f86
Sahadeb Barman <saha.ptc.om@gmail.com>
2018-05-29 11:06:15 +0530

    testing

--- API/module/RequestType/getPnrStatus.js
+++ API/module/RequestType/getPnrStatus.js
@@ -11,7 +11,25 @@ module.exports.getPnrStatus = function getPnrStatus(request,passtoHandler){
      + getDataFromConstantFile.API_KEY_1 + '/';
      fromRailWayAPI.callTheRailwayApi(URL,(getResponseFromAPI)=>{
         let getJsonData = JSON.parse(getResponseFromAPI);
-        passtoHandler(getJsonData);
+        trainName=getJsonData.train.name;
+        trainNumber=getJsonData.train,number;
+        pnrNumber=getJsonData.pnr;
+        pnrCurrentStatus=getJsonData.passengers[0].current_status;
+        pnrBookingStatus=getJsonData.passengers[0].booking_status;
+       // noOfPerson=
+        dateOfJourney=getJsonData.doj;
+        boardinPoint=getJsonData.boarding_point.name;
+        toStation=getJsonData.to_station.name;
+        fromStation=getJsonData.from_station.name;
+
+        outPutToEndUser={
+        fulfillmentText:"The current status of PNR number "+pnrNumber+", on train "+trainName+"("
+                         +trainNumber+") from "+fromStation+" with having boarding points from "+boardinPoint
+                         +", to "+toStation+" and date of journey "+dateOfJourney+", current seat status is "
+                         +pnrCurrentStatus+" as compared to booking status "+pnrBookingStatus+" "
+        }
+
+        passtoHandler(outPutToEndUser);
      })
 
 }
